d20700c582f6316001876d2fa586767caea4a9fe
Yohei Okaya <okaya.dcinc@gmail.com>
2020-07-13 15:22:51 +0900

    code

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "example-service"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.2.0" % "provided"
+
---
+++ src/main/scala/Hello.scala
@@ -0,0 +1,14 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Hello {
+  def main(args: Array[String]): Unit = {
+
+    val conf = new SparkConf().setAppName("Simple Application")
+    val sc = new SparkContext(conf)
+    val rdd = sc.range(0, 100, 1, 10)
+    println("----Start----")
+    rdd.map(i => i*2)
+      .foreach(i => println(i))
+  }
+}
+
